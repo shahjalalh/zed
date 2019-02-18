53648f49bd4267384600a06fe10f093c1a7891be
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2019-02-18 12:23:33 +0600

    landing page

--- zed/urls.py
+++ zed/urls.py
@@ -18,8 +18,11 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from .views import HomePageView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomePageView.as_view(), name='home'),
 ]
 
 if settings.DEBUG:
