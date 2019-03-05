3011a7d8fadba61a241953ee26a02fab951bc8d1
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2019-03-05 08:58:16 +0600

    user admin list display

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 from .models import User
+
 
 # Register your models here.
 
-admin.site.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'first_name', 'last_name', 'email', 'is_staff', 'is_active', 'created_date', 'modified_date')
+
+admin.site.register(User, UserAdmin)
