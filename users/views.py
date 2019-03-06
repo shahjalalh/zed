cea4a1015c3ae521e74c78b9f3a6aa6264713f46
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2019-03-06 09:22:35 +0600

    user profile view cireated

--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic.base import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import DetailView
@@ -6,14 +6,23 @@ from .models import User
 
 # Create your views here.
 
-"""
-class UserProfileView(LoginRequiredMixin, TemplateView):
-    login_url = "/accounts/login/"
-    template_name = "accounts/profile.html"
-"""
 
 class UserDetailView(LoginRequiredMixin, DetailView):
-
+    
     model = User
     slug_field = "username"
     slug_url_kwarg = "username"
+    template_name = "account/user_detail.html"
+
+    def get_object(self):
+        
+        object = get_object_or_404(User, username=self.kwargs.get("username"))
+
+        # only owner can view his page
+        if self.request.user.username == object.username:
+            return object
+        else:
+            # redirect to 404 page
+            print("you are not the owner!!")
+
+        
--- zed/urls.py
+++ zed/urls.py
@@ -22,8 +22,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('user/', include('users.urls')),
     path('', include('home.urls')),
-    path('/user/', include('users.urls')),
 ]
 
 if settings.DEBUG:
